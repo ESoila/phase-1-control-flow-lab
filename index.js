8787574fa2057136c23c4cdbe150d7c62ea56f3f
soila <elizabeth.soila@student.moringaschool.com>
2024-12-06 16:48:37 +0300

    phase-1-control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// scuberGreetingForFeet() function
+function scuberGreetingForFeet(Value) {
+  if (Value <= 400) {
+    return "This one is on me!";
+  } else if (Value >=401  && Value <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (Value > 2000 && Value <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (Value > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// ternaryCheckCity() function
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// switchOnCharmFromTip() function
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
+
+
